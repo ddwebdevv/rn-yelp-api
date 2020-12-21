d448567dabe0de63323235b0e4012c4a4a1d94c8
Dmitry Dergunov <dmitrynuc@gmail.com>
2020-12-20 23:06:57 -0500

    cleaning key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import yelpKey from '../../authorization';
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer dXvZlD8uPmXWzYUafiEElZnoba3MTBdm7Ym3kR_JoDvRiov26npgTEqtEWudGQf4Htd5TxWVgbGWe-4oYoG51Rq6JsP9aq2-GKMy3AZU_yVvHbIm0t5UtIR_bpXeX3Yx'
+        Authorization: {yelpKey}
     }
 });
